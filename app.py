dedeb89202f7e01a7b30491eaa1bb9893c63683c
James Schiiller <jgschiiller@gmail.com>
2017-01-27 10:20:14 -0500

    y

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World from %s' % socket.gethostname()
+    return 'Hello Docker World from %s' % socket.gethostname()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
